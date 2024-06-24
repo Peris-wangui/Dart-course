1aea26580095078f03634ba07871a286d577bb4c
Peris-wangui <perismunene23@gmail.com>
2024-06-24 16:32:19 +0300

    Intro to dart

---
+++ plp.dart
@@ -0,0 +1,61 @@
+void main() {
+  //data types
+  //int ---> number --> whole numbers
+  //double --> number --> decimal numbers
+  //string --> text --> Pepe
+  //booline --> true or false
+  //var isTrue = true;
+  //variables
+
+  //declaration of a variable
+  //data_type variable_name
+  //int age = 24;
+  //print(age);
+  //string declaration
+  //const name = "Pepe";
+  //final country = "Kenya";
+  //print(name);
+  //print(age);
+  //constants
+  //print("Hello, my name is $name and i am $age years old and i am from $country");
+  //var age = 27;
+  //var age2 = 32;
+  //print(age2 > age);
+  //print(90 > 80);
+
+  //conditional statements
+  // if statement
+  //if ... else statement
+  /*
+  var people = 10;
+  if (people < 3) {
+    print("The event will not continue");
+    //The session will continue -->
+  } else {
+    print("The session will continue");
+  }
+  */
+  //if... else if... else statement
+  /*
+  var marks = 70;
+  if (marks >= 80 && marks <= 100) {
+    print("A");
+  } else if (marks >= 70 && marks < 80) {
+    print(" You got Grade B of $marks marks");
+  }
+  */
+  //switch statement
+  //switch(variable) { case value: break; default: }
+
+  /*var marks = 70;
+  switch (marks) {
+    case 80:
+      print("A");
+      break;
+    case 70:
+      print("B");
+    default:
+      print("Invalid Marks");
+  }
+  */
+}
